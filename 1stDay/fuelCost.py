8dc82f78d5f018be2e3430886008b36f1c0e45ac
Ian <iancross216@gmail.com>
2019-12-12 13:01:27 -0500

    Completed Day1: Fuel Costs by Module Weight

---
+++ 1stDay/fuelCost.py
@@ -0,0 +1,25 @@
+import sys
+from pathlib import Path
+from math import floor
+
+input_file = Path("input.txt")
+
+# weights = [(floor(int(line.strip())/3)-2) if line.strip().isnumeric() else 0 for line in input_file.open()]
+module_weights = []
+curr_weight = []
+
+def fuel_cost(weight):
+    curr_cost = floor(weight/3)-2
+    if (curr_cost >= 0):
+        curr_weight.append(curr_cost)
+        fuel_cost(curr_cost)
+
+
+for line in input_file.open():
+    line = line.strip()
+    if line.isnumeric():
+        fuel_cost(int(line))
+        module_weights.append(sum(curr_weight))
+        curr_weight = []
+
+print(sum(module_weights))
---
+++ README.md
@@ -0,0 +1,9 @@
+# Advent of Code
+
+Created by [Eric Wastl](http://was.tl/) Advent of Code is an Advent calendar of small programming puzzles for a variety of skill sets and skill levels. Being released every day in the Month of December 2019 this is purely for fun over the holiday break.
+
+Follow along and play for yourself at [Advent of Code](https://adventofcode.com/2019)
+
+### Author
+
+**Ian Cross**
