c782831774a0fe0d5f3c054db5e09545fca85bd0
Ian-Cross <iancross216@gmail.com>
2022-12-01 23:28:13 -0500

    Time for advent 🎁

--- reused/devLoop.py
+++ reused/devLoop.py
@@ -1,102 +1,102 @@
-import sys, os
-from reused.setup.setup import setup, get_test_answer, make_readme
-import subprocess
+import sys
+import os
+import inspect
 from importlib import import_module, reload
-import requests
+
 from bs4 import BeautifulSoup
+import requests
+
+from reused.setup.setup import setup, get_test_answer, make_readme
 
 year = 0
 day = 0
+
+
 def submit(part, answer):
-  print(f"Submiting {answer} to part {part}")
-  session = os.environ["session"]
-  response = requests.post(
-    f'https://adventofcode.com/{year}/day/{int(day)}/answer',
-    data={'level': part, 'answer': answer},
-    headers={"cookie": f"session={session};"},
-  )
+    print(f"Submiting {answer} to part {part}")
+    session = os.environ["session"]
+    response = requests.post(
+        f'https://adventofcode.com/{year}/day/{int(day)}/answer',
+        data={'level': part, 'answer': answer},
+        headers={"cookie": f"session={session};"},
+    )
 
-  soup_aisle = BeautifulSoup(response.text,features="lxml")
-  soup_cans = soup_aisle.find_all('article')
-  for can in soup_cans:
-    resp = can.text.split(".")[0]
-    if ("That's the right answer!" in resp):
-      return True
-  return False
-  
-
-def part_1():
-  key = '.'
-  while (key.lower() != 'q'):
-    key = input("Enter command: ")
-
-    try:
-      if key == "s":
-        setup(year,day)
-      elif key[0] == "t":
-        test_answer = get_test_answer(year,day, 1)
-        pyday = import_module(f"{year}.day{day}.main")
-        pyday = reload(pyday)
-        calculated_answer = pyday.part1(f"{year}/day{day}/test.txt")
-
-        if (str(calculated_answer) == str(test_answer)):
-          print("Test passed")
-
-          if len(key) > 1:
-            print(pyday.part1(f"{year}/day{day}/input.txt"))
-          else:
-            if (submit(1,pyday.part1(f"{year}/day{day}/input.txt"))):
-              print("Submission Successful, moving to part 2")
-              key = 'q'
-            else:
-              print("Submission failed, take a closer look.")
-        else:
-          print(f"Test failed, expected {test_answer}, got {calculated_answer}")
-    except Exception as e:
-      print("Errored out")
-      print(e)
+    soup_aisle = BeautifulSoup(response.text, features="lxml")
+    soup_cans = soup_aisle.find_all('article')
+    for can in soup_cans:
+        resp = can.text.split(".")[0]
+        if ("That's the right answer!" in resp):
+            return True
+    return False
 
 
-def part_2():
-  key = '.'
-  while (key.lower() != 'q'):
-    key = input("Enter command: ")
+def testSolution(part, year, day, test_answer=None):
+    if test_answer == None:
+        test_answer = get_test_answer(year, day, part)
 
     pyday = import_module(f"{year}.day{day}.main")
+    pyday = reload(pyday)
 
-    try:
-      if key == "s":
-        make_readme(year,day)
-      elif key[0] == "t":
-        test_answer = get_test_answer(year,day,2)
-        pyday = reload(pyday)
-        calculated_answer = pyday.part2(f"{year}/day{day}/test.txt")
+    calculated_answer = getattr(pyday, f"part{part}")(
+        f"{year}/day{day}/test.txt")
 
-        if (str(calculated_answer) == str(test_answer)):
-          print("Test passed")
+    if (str(calculated_answer) == str(test_answer)):
+        print("Test passed")
 
-          if (len(key) > 1):
-            print(pyday.part2(f"{year}/day{day}/input.txt"))
-          else:
-            if (submit(2,pyday.part2(f"{year}/day{day}/input.txt"))):
-              print("Submission Successful! Nice Work")
-              key = 'q'
-            else:
-              print("Submission failed, take a closer look.")
+        if (submit(part, getattr(pyday, f"part{part}")(f"{year}/day{day}/input.txt"))):
+            print("Submission Successful, moving on")
+            return True
         else:
-          print(f"Test failed, expected {test_answer}, got {calculated_answer}")
-    except Exception as e:
-      print("Errored out")
-      print(e)
+            print("Submission failed, take a closer look.")
+            return False
+    else:
+        print(
+            f"Test failed, expected {test_answer}, got {calculated_answer}")
+        return False
 
-def main(y,d):
-  global year,day
-  year = y
-  day = d
 
-  part_1()
-  part_2()
+def execute(part, year, day):
+    pyday = import_module(f"{year}.day{day}.main")
+    pyday = reload(pyday)
+
+    print(getattr(pyday, f"part{part}")(
+        f"{year}/day{day}/test.txt"))
+
+
+def debug(file=None, function=None, params=None):
+    print(params)
+    fn = import_module(file)
+    fn = reload(fn)
+    print(getattr(fn, function)(*params))
+
+
+def develop(part):
+    keys = ['.']
+    while (keys[0].lower() != 'q'):
+        keys = input(f"[P{part}] Enter command: ").split(" ")
+        try:
+            if keys[0] == "s":
+                setup(year, day)
+            elif keys[0] == "t":
+                testSolution(part, year, day, *keys[1:])
+            elif keys[0] == 'i':
+                execute(part, year, day)
+            elif keys[0] == 'd':
+                debug(keys[1], keys[2], keys[3:])
+
+        except Exception as e:
+            print("Errored out")
+            print(e)
+
+
+def main(y, d):
+    global year, day
+    year = y
+    day = d
+
+    develop(1)
+    develop(2)
 
 
 if __name__ == "__main__":
-  main(sys.argv[1],sys.argv[2])
+    main(sys.argv[1], sys.argv[2])
