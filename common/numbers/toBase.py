da34838ac2b3e01c97df1cc58d1fde0f16b81577
Ian-Cross <iancross216@gmail.com>
2023-12-26 14:09:40 -0330

    Discovered the condition of springs ♨️

--- common/numbers/toBase.py
+++ common/numbers/toBase.py
@@ -5,6 +5,8 @@ def to_base(num,b):
   base_keys = { 0: '0', 1: '1', 2: '2', 3: '3', 4:'4', 5:'5', 6:'6', 7:'7', 8:'8', 9:'9', 10: 'A', 11: 'B', 12: 'C', 13: 'D', 14: 'E', 15: 'F', 16: 'G', 17: 'H' }
 
   ans = []
+  if num == 0:
+     return '0'
   while num > 0:
       ans.append(base_keys[num % b])
       num = int(num / b)
