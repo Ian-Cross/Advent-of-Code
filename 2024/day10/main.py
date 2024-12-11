3014e67a34f3961d613c479daa2b2ca90d14bd0c
Ian-Cross <iancross216@gmail.com>
2024-12-11 10:15:34 -0330

    Cleaning 🧹

--- 2024/day10/main.py
+++ 2024/day10/main.py
@@ -8,36 +8,18 @@ PATH = "2024/day10/test.txt"
 trailheads = []
 seen = []
 topi_map = None
-count = 0
-
-class trail_marker:
-  value = -1
-  pos = None
-  parent = None
-  children = []
-  len_trail = -1
-
-  def __init__(self,value,pos, parent = None, children = []):
-    self.value = value
-    self.pos = pos
-    self.parent = parent
-    self.children = children
 
 def take_step(marker,p2 = False):
   global seen
-  if topi_map.at(marker.pos).data == "9":
-    if p2 or marker.pos not in seen:
-      seen.append(marker.pos)
+  if topi_map.at(marker).data == "9":
+    if p2 or marker not in seen:
+      seen.append(marker)
     return
 
   for neighbour in CARDINAL_NEIGHBOURS:
-    new_pos = add_tup(marker.pos,neighbour)
-    if not topi_map.in_bounds(new_pos):
-      continue
-    if int(topi_map.at(new_pos).data) - 1 == int(topi_map.at(marker.pos).data):
-      new_marker = trail_marker(topi_map.at(new_pos).data,new_pos,marker,[])
-      marker.children.append(new_marker)
-      take_step(new_marker,p2)
+    new_pos = add_tup(marker, neighbour)
+    if topi_map.in_bounds(new_pos) and int(topi_map.at(new_pos).data) - 1 == int(topi_map.at(marker).data):
+      take_step(new_pos, p2)
 
 def find_trailheads(row,row_num,col,col_num):
   global trailheads
@@ -52,13 +34,11 @@ def part1(path):
 
   trails = []
   for trailhead in trailheads:
-    head = trail_marker(topi_map.at(trailhead).data, trailhead, None, [])    
-    take_step(head)
-    head.len_trail = len(seen)
-    trails.append(head)
+    take_step(trailhead)
+    trails.append(len(seen))
     seen = []
 
-  return sum([trail.len_trail for trail in trails])
+  return sum(trails)
 
 
 def part2(path):
@@ -69,13 +49,11 @@ def part2(path):
 
   trails = []
   for trailhead in trailheads:
-    head = trail_marker(topi_map.at(trailhead).data, trailhead, None, [])    
-    take_step(head, p2=True)
-    head.len_trail = len(seen)
-    trails.append(head)
+    take_step(trailhead, p2=True)
+    trails.append(len(seen))
     seen = []
     
-  return sum([trail.len_trail for trail in trails])
+  return sum(trails)
 
 if __name__ == "__main__":
   arguments(part1, part2)
